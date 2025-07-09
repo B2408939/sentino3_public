cfc6153413c4d0501c069a4b4b6de078cb3cd300
B2408939 <tcaoanhvo@gmail.com>
2025-07-09 09:28:58 +0700

    Update state.py

--- Test/state.py
+++ Test/state.py
@@ -2,4 +2,4 @@
 model_names = ["Logistic Regression", "Decision Tree", "Random Forest", "SVM", "KNN"]
 model_hyperparams = {name: {} for name in model_names}
 df = None
-split_config = {}  # chứa {"method": "Train/Test Split", "params": {"test_size": 0.2, "random_state": 42}}
+split_config = {}  # Include: {"method": "Train/Test Split", "params": {"test_size": 0.2, "random_state": 42}}
